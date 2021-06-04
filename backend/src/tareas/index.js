fabcd9e21a4df2257d7726c9d49e059c630b8bcc
Lucero Amaolo <lamaolo.m@gmail.com>
2021-06-04 12:28:24 -0300

    Primer endpoint agregado

--- backend/src/tareas/index.js
+++ backend/src/tareas/index.js
@@ -3,6 +3,11 @@ const tareasAPI = require("express").Router();
 
 /* ENDPOINTS DE tareasRouter AQUI */
 
+// -> localhost:3000/api/tareas
+tareasRouter.get("/", (req, res) => {
+  res.status(200).json({ error: null, data: "Estas en /api/tareas!" });
+});
+
 /* ENDPOINTS DE tareasRouter AQUI */
 
 tareasAPI.use("/tareas", tareasRouter);
