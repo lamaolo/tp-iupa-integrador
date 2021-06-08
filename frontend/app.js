1228815b17f32112a8d5f35ae41b886d0079d2e9
Arcaytus <manu.martinezgalan@gmail.com>
2021-06-08 20:08:49 -0300

    agregada funcionalidad para crear user

--- frontend/app.js
+++ frontend/app.js
@@ -8,7 +8,7 @@ const templateRow = document.getElementById('contentRow').content;
 
 const inputName = document.getElementById('inputName');
 const inputNameHelp = document.getElementById('inputNameHelp');
-const inputNameFormGroup = inputUsernameHelp.parentElement;
+const inputNameFormGroup = inputNameHelp.parentElement;
 const inputSurname = document.getElementById('inputSurname');
 const inputSurnameHelp = document.getElementById('inputSurnameHelp');
 const inputSurnameFormGroup = inputSurnameHelp.parentElement;
@@ -19,15 +19,14 @@ const createUserFormContent = document.getElementById('createUser');
 
 
  //AGREGAR ROW
- @param {*} nombre
- @param {*} apellido
+
  
 
- function addRow(id, nombre, apellido, task) {
+ function addRow(id, nombre, apellido, tarea) {
  const row =templateRow.cloneNode(true);
 
   // Modifico el valor del nodo de texto por el ingesado por el usuario
-row.querySelector('.txtTask').innerText = task;
+row.querySelector('.txtTarea').innerText = tarea;
 row.querySelector('.txtDescription').innerText = description;
 
 
@@ -41,42 +40,17 @@ contentTable.appendChild(row);
 
 
 // Cargar datos de la tabla
-// async function loadTable() {
-//  if (localStorage.getItem('token')) {
-//    contentTable.innerHTML = '';
-//    const data = await api('get', '/users');
-//    data.forEach(({ name, age, id, tasks }) => addRow(name, age, id, tasks));
-//  }
-// }
+ async function loadTable() {
+  if (localStorage.getItem('token')) {
+    contentTable.innerHTML = '';
+    const data = await api('get', '/users');
+    data.forEach(({ name, age, id, tasks }) => addRow(name, age, id, tasks));
+  }
+ }
 
 // Inicio de la APP.
 async function initApp() {
   await loadTable();
-}
-
-// Crear usuario.
-
-async function createUser() {
-  const nombre = inputNombre.value;
-  const apellido = inputApellido.value;
-  const email = inputEmail.value;
-  const password = inputPassword.value;
-
- resetFormErrors();
-
-  const response = await api('post', '/users', {
-    name,
-    apellido,
-    password,
-    email,
- });
-
-  if (response.errors) {
-    updateFormErrors(response.errors);
- } else {
-   createUserForm.reset();
-   loadTable();
-  }
 }
 
 
--- frontend/login.js
+++ frontend/login.js
@@ -1,7 +1,9 @@
 const formLogin = document.getElementById('login');
+const regName = document.getElementById('name');
+const regSname = document.getElementById('surname');
 const loginUser = document.getElementById('username');
 const writePass = document.getElementById('password');
-const lOut = document.getElementsById('logOut');
+const lOut = document.getElementById('logOut');
 
 // Determina si el usuario está conectado
 function isLoggedIn() {
@@ -31,7 +33,7 @@ function isLoggedIn() {
   //Funcion para conectarse
   async function login() {
     const username = loginUser.value;
-    const password = loginPassword.value;
+    const password = writePass.value;
   
     const response = await api('post', '/login', { username, password });
   
@@ -49,3 +51,23 @@ function isLoggedIn() {
   }
   
   updateLoginStatus();
+
+
+async function createUser() {
+  const create = ({ nombre, apellido, email, password }) => {
+    if (!email || !password || !nombre || !apellido) {
+      return Promise.reject({
+        message:
+          "Los campos `nombre`, `apellido`, `email` y `password` son obligatorios.",
+      });
+    }
+  
+    return new Promise((resolve, reject) => {
+      db.registerUser({ nombre, apellido, email, password })
+        .then(([usuarioCreado]) => resolve(usuarioCreado))
+        .catch((error) => reject(error));
+    });
+  };
+}
+
+
