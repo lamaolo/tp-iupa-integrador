9ae0f064c45e844977daa88e8c8ed2dceefde89c
Lucero Amaolo <lamaolo.m@gmail.com>
2021-06-13 13:26:46 -0300

    Anadido middleware para verificar JWT

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const [bearer, token] = authHeader.split(" ");
+
+    if (!token) {
+      res.status(401).json({
+        error: "Acceso denegado.",
+        data: null,
+      });
+    } else {
+      try {
+        const verifyToken = jwt.verify(token, JWT_SECRET);
+
+        console.log("VERIFY TOKEN: ", verifyToken);
+
+        // escribo el resultado del token en el objeto request
+        req.user = verifyToken;
+        next();
+      } catch (error) {
+        res.status(401).json({
+          error: "Token invalido.",
+          data: null,
+        });
+      }
+    }
+  } else {
+    res.status(401).json({
+      error:
+        "Acceso denegado. Debes enviar el JWT en el header `authorization` con la sintaxis `Bearer {token}`.",
+      data: null,
+    });
+  }
+};
+
+module.exports = verifyJWT;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const apiRouter = require("express").Router();
-
+const verifyToken = require("../middleware/verifyJWT");
 const usuariosAPI = require("./usuarios");
 const tareasAPI = require("./tareas");
 
 apiRouter.use("/api", usuariosAPI);
-apiRouter.use("/api", tareasAPI);
+
+// Middleware de verifyToken: Todas las request que se hagan a /api/usuarios deben enviar un token
+apiRouter.use("/api", verifyToken, tareasAPI);
 
 module.exports = apiRouter;
