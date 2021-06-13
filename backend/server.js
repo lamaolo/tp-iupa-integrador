08ea4090a7d30dab6f251b5e5e5662860e29a879
Lucero Amaolo <lamaolo.m@gmail.com>
2021-06-13 15:29:23 -0300

    Anadidas configuraciones para deploy

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,29 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config");
 const apiRouter = require("./src");
 const db = require("./src/database");
+const fs = require("fs");
 
 const app = express();
-// const db = new Database();
 
 app.use(bodyParser.json());
 app.use(apiRouter);
+
+// autogenerar archivo config.js para modo de produccion
+if (process.env.NODE_ENV === "production") {
+  fs.writeFileSync(
+    "./config.js",
+    `
+  module.exports = {
+    PORT: 3000,
+    DB_NAME: "${process.env.DB_NAME}",
+    DB_HOST: "${process.env.DB_HOST}",
+    DB_PASSWORD: "${process.env.DB_PASSWORD}",
+    DB_USER: "${process.env.DB_USER}",
+    JWT_SECRET: "${process.env.JWT_SECRET}",
+  };
+  `
+  );
+}
 
 (async function initApp() {
   try {
